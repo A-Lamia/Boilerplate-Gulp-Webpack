d48d83618c78cb52464a15c8c9a8e1cc6aacc84c
Abdulah Lamia <admin@lamia.io>
2017-06-20 05:03:01 +1000

    Test changes.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,9 @@
-function getName(name) {
-  return console.log(`Hello ${name} how are you!`);
-}
-
-getName('smith');
-
-const check = () => {
-  return console.log('Enter random text');
-};
+const check = () => console.log('Testing 1.. 2..');
 
 check();
+
+function getName(name) {
+  return console.log(`${name}, HMR is running great!!`);
+}
+
+getName('My dude');
